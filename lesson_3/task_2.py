5d26489778188b5dab6775c19a58dd1a07dae715
Nikita Kanushin <kanushinn@gmail.com>
2022-02-07 19:38:08 +0600

    completed the task_2

---
+++ lesson_3/task_2.py
@@ -0,0 +1,4 @@
+def data_func(firstname, lastname, bdate, city, email, phone):
+    print(f'Имя - {firstname}, Фамилия - {lastname}, День рождения - {bdate}, Город проживания - {city}, Email - {email}, Телефон - {phone}')
+
+data_func(firstname='Никита', lastname='Канушин', bdate='22.11.1995', city='Омск', email='email@email.com', phone='Iphone 11')
