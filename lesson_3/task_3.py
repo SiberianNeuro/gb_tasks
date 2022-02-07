129e1e4974fc470dcc3680f03c8d9c7029d29a4a
Nikita Kanushin <kanushinn@gmail.com>
2022-02-07 19:52:52 +0600

    repaired the task_3

--- lesson_3/task_3.py
+++ lesson_3/task_3.py
@@ -5,7 +5,7 @@ def my_func():
         var_3 = int(input('Введите третье число: '))
     except ValueError:
         return 'Можно вводить только числа!'
-        if var_1 < var_2 and var_1 < var_3:
+    if var_1 < var_2 and var_1 < var_3:
         var_sum = var_2 + var_3
     elif var_2 < var_1 and var_2 < var_3:
         var_sum = var_1 + var_3
