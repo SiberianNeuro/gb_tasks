9c08079f91574ad76a1586446a41ea2a96d3f8f8
Nikita Kanushin <kanushinn@gmail.com>
2022-02-07 21:09:34 +0600

    reworked the task_5

--- lesson_3/task_5.py
+++ lesson_3/task_5.py
@@ -3,19 +3,19 @@
 # Пользователь может продолжить ввод чисел, разделённых пробелом и снова нажать Enter.
 # Сумма вновь введённых чисел будет добавляться к уже подсчитанной сумме.
 
-
-my_list = input('Введите числа через пробел: ').split(' ')
-int_list = []
-for el in my_list:
-    int_list.append(int(el))
-int_list_sum = sum(int_list)
-print(f'Получившаяся сумма - {int_list_sum}')
-while input('Ввести еще один набор чисел? да/нет: ') == 'да':
-    new_list = input('Введите числа через пробел: ').split(' ')
-    for elem in new_list:
-        int_list.append(int(elem))
-    int_list_sum = sum(int_list)
+def sum_func():
+    my_list = input('Введите числа через пробел: ').split(' ')
+    int_list = []
+    for el in my_list:
+        int_list.append(int(el))
+        int_list_sum = sum(int_list)
     print(f'Получившаяся сумма - {int_list_sum}')
+    while input('Ввести еще один набор чисел? да/нет: ') == 'да':
+        new_list = input('Введите числа через пробел: ').split(' ')
+        for elem in new_list:
+            int_list.append(int(elem))
+            int_list_sum = sum(int_list)
+        print(f'Получившаяся сумма - {int_list_sum}')
+    return int_list_sum
 
-print(f'Программа завершена, последняя сумма чисел - {int_list_sum}')
-
+print(f'Итоговая сумма чисел - {sum_func()}')
