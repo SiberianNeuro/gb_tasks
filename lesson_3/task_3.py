016d87a54c7974308f441321de1b1a7933e24cfc
Nikita Kanushin <kanushinn@gmail.com>
2022-02-07 19:51:48 +0600

    completed the task_3

---
+++ lesson_3/task_3.py
@@ -0,0 +1,16 @@
+def my_func():
+    try:
+        var_1 = int(input('Введите первое число: '))
+        var_2 = int(input('Введите второе число: '))
+        var_3 = int(input('Введите третье число: '))
+    except ValueError:
+        return 'Можно вводить только числа!'
+        if var_1 < var_2 and var_1 < var_3:
+        var_sum = var_2 + var_3
+    elif var_2 < var_1 and var_2 < var_3:
+        var_sum = var_1 + var_3
+    elif var_3 < var_1 and var_3 < var_2:
+        var_sum = var_1 + var_2
+    return var_sum
+
+print(my_func())
