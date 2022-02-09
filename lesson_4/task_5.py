2b7c7db81bb8a9460b8b4858ed3c5f21cc0cbe96
Nikita Kanushin <kanushinn@gmail.com>
2022-02-09 15:12:24 +0600

    completed the task_5

--- lesson_4/task_5.py
+++ lesson_4/task_5.py
@@ -2,3 +2,12 @@
 # В список должны войти чётные числа от 100 до 1000 (включая границы).
 # Нужно получить результат вычисления произведения всех элементов списка.
 # Подсказка: использовать функцию reduce().
+
+from functools import reduce
+
+my_list = [i for i in range(100, 1001) if i % 2 == 0]
+
+def my_func(prev_el, el):
+    return prev_el * el
+
+print(reduce(my_func, my_list))
