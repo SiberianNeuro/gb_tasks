a47c97496ae138c930834bbdfe88211eec286aa5
Nikita Kanushin <kanushinn@gmail.com>
2022-02-13 20:55:50 +0600

    completed the task_3

--- lesson_5/task_3/task_3.py
+++ lesson_5/task_3/task_3.py
@@ -2,11 +2,23 @@
 # Определить, кто из сотрудников имеет оклад менее 20 тысяч, вывести фамилии этих сотрудников.
 # Выполнить подсчёт средней величины дохода сотрудников.
 # Пример файла:
-#
 # Иванов 23543.12
 # Петров 13749.32
 
-f_obj = open("task_file.txt", 'r')
-for line in f_obj:
-    print(line)
-f_obj.close()
+workers_lastname = []
+workers_counter = 0
+salary_summ = 0
+with open("task_file.txt") as f_obj:
+    for line in f_obj:
+        print(line)
+        worker_salary = line.split(' ')
+        if float(worker_salary[1]) < 20000.00:
+            workers_lastname.append(worker_salary[0])
+        salary_summ += float(worker_salary[1])
+        workers_counter += 1
+print(f'Средняя величина дохода сотрудников - {salary_summ / workers_counter:.2f} р.')
+print('Сотрудники с доходом выше 20 000 р.:')
+for ind, el in enumerate(workers_lastname, 1):
+    print(ind, el)
+
+# При попытке создать файл на кириллице возникает ошибка декодирования, не смог решить данный вопрос.
