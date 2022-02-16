ee6dcffdcae50ec6a85b7621482a463a1344fd1d
Nikita Kanushin <kanushinn@gmail.com>
2022-02-16 16:35:19 +0600

    completed the task_5

--- lesson_6/task_5.py
+++ lesson_6/task_5.py
@@ -3,3 +3,47 @@
 # создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер);
 # в каждом классе реализовать переопределение метода draw. Для каждого класса метод должен выводить уникальное сообщение;
 # создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+
+class Stationery:
+
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f'{self.title}: запуск отрисовки')
+
+class Pen(Stationery):
+
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        print(f'{self.title}: запуск отрисовки ручкой')
+
+class Pencil(Stationery):
+
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        print(f'{self.title}: запуск отрисовки карандашом')
+
+class Handle(Stationery):
+
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        print(f'{self.title}: запуск отрисовки маркером')
+
+stationary = Stationery('Канцелярская принадлежность')
+stationary.draw()
+
+pen = Pen('Ручка')
+pen.draw()
+
+pencil = Pencil('Карандаш')
+pencil.draw()
+
+handle = Handle('Маркер')
+handle.draw()
